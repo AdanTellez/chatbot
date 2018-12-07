de1c20b03899885f66a48348829a137d0307e621
AdanTellez <40607980+AdanTellez@users.noreply.github.com>
2018-12-06 20:33:13 -0600

    Update index.blade.php

--- resources/views/mensajes/index.blade.php
+++ resources/views/mensajes/index.blade.php
@@ -4,11 +4,10 @@
 
 <h1>Solicitudes de mensajes</h1>
 <a href="{{ route('mensajes.create')}}">Nueva solicitud</a>
-<table width="80%" border="1">
+<table width="50%" border="1">
 	<thead>
 		<tr>
 			<th>Titulo</th>
-			<th>Mensaje</th>
 			<th>Status</th>
 			<th>Fecha</th>
 		</tr>
@@ -16,11 +15,10 @@
 	<tbody>
 		@foreach ($mensajes as $mensaje)
 			<tr>
-				<td>{{$mensaje->Mensaje_titulo}}</td>
-				<td>{{$mensaje->Mensaje_mensaje}}</td>
+				<td width="50%">{{$mensaje->Mensaje_titulo}}</td>
 				<td>{{$mensaje->Mensaje_estatus}}</td>
 				<td>{{$mensaje->Mensaje_fecha}}</td>
-				<td>
+				<td width="20%">
 					<a href="{{ route('mensajes.edit', $mensaje->Mensaje_id )}}">Editar</a>
 					<form style="display: inline;" method="POST" action="{{route('mensajes.destroy', $mensaje->Mensaje_id)}}">
 						{!! method_field('DELETE') !!}
